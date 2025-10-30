0d94a93723cf0941110aa85da2711e3cb44724ba
Alberto Cesar <albertocesar.be@gmail.com>
2025-10-30 09:35:44 -0300

    feat: criação model usuário

---
+++ agendamento-back/models/userModel.js
@@ -0,0 +1,39 @@
+const { DataTypes, Model } = require('sequelize')
+const db = require('../config/db')
+
+class User extends Model {};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        role: {
+            type: DataTypes.ENUM,
+            values: ['common', 'admin', 'staff'],
+            allowNull: false,
+            defaultValue: 'common'
+        }
+    },
+    {
+        sequelize: db,
+        modelName: 'User'
+    }
+);
+
+module.exports = User
