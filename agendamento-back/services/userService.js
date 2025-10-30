b943a944b4cb7c663c7ecdbb4aa495ecfcd460ce
Alberto Cesar <albertocesar.be@gmail.com>
2025-10-30 15:08:28 -0300

    feat: criando service para crud usuário

---
+++ agendamento-back/services/userService.js
@@ -0,0 +1,72 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+
+const SALT_ROUNDS = 10;
+
+exports.createUser = async function(userDTO) {
+    try {
+        const hashedPassword = await bcrypt.hash(userDTO.password, SALT_ROUNDS);
+        userDTO.password = hashedPassword;
+        return (await User.create(userDTO)).toJSON();
+    } catch(error) {
+        throw new Error('Erro ao cadastrar usuário: ' + error.message)
+    }
+}
+
+exports.updateUser = async function(id, userDTO) {
+    try {
+        await User.update(userDTO, { where: { id: id } });
+        const updatedUser = await User.findByPk(id);
+        return updatedUser ? updatedUser.toJSON() : null;
+    } catch(error) {
+        throw new Error('Erro ao atualizar usuário: ' + error.message)
+    }
+}
+
+exports.deleteUser = async function(id) {
+    try {
+        const deleted = await User.destroy({ where: { id: id } });
+        return deleted > 0;
+    } catch(error) {
+        throw new Error('Erro ao deletar usuário: ' + error.message)
+    }
+}
+
+exports.findUserById = async function(id) {
+    try {
+        const user = await User.findByPk(id);
+        return user ? user.toJSON() : null;
+    } catch(error) {
+        throw new Error('Erro ao buscar usuário: ' + error.message)
+    }
+}
+
+exports.findUserByEmail = async function(email) {
+    try {
+        const user = await User.findOne({ where: { email: email } })
+        return user ? user.toJSON() : null;
+    } catch(error) {
+        throw new Error('Erro ao buscar usuário: ' + error.message)
+    }
+}
+
+
+exports.createUserDTO = function(userData) {
+    return {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password
+    }
+}
+
+exports.simpleUserDTO = function(userData) {
+    return {
+        name: userData.name,
+        email: userData.email
+    }
+}
+
+
+
+
+
