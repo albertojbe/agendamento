dffb55e28f1cb6084981015ba6581a1563c8fc65
Alberto César <albertocesar.be@gmail.com>
2025-11-16 08:05:28 -0300

    feat: implementando estilo dinamico nos links

--- agendamento-front/src/views/Home.vue
+++ agendamento-front/src/views/Home.vue
@@ -11,6 +11,12 @@ function handleLogout() {
   router.push('/login');
 }
 
+function linkClass(route) {
+  return router.currentRoute.value.path === route
+    ? 'text-bg-soft-neutral'
+    : '';
+}
+
 onMounted(() => {
   const token = localStorage.getItem('authToken');
   if (!token) {
@@ -30,8 +36,10 @@ onMounted(() => {
 
 <template>
   <div data-theme="light" class="bg-base-200" style="height: 100vh;">
-    <nav j class="navbar rounded-box flex w-full items-center justify-between gap-2 shadow-base-300/20 shadow-sm px-40">
-      <div class="navbar-start max-md:w-1/4">
+    <nav j
+      class="navbar rounded-b-2xl flex w-full items-center justify-between gap-2 shadow-base-300/20 shadow-sm px-40">
+      <div class="navbar-start flex flex-row items-center max-md:w-1/4">
+        <span class="icon-[tabler--calendar] size-8 mx-4"></span>
         <a class="link text-base-content link-neutral text-xl font-bold no-underline" href="#">
           Agendamento
         </a>
@@ -39,16 +47,21 @@ onMounted(() => {
       <div class="navbar-end max-md:hidden">
         <ul class="menu menu-horizontal p-0 font-medium">
           <li>
-            <RouterLink to="/agendamentos">Agendamentos</RouterLink>
+            <RouterLink :class="linkClass('/agendamentos')" to="/agendamentos">Agendamentos</RouterLink>
           </li>
           <li>
-            <RouterLink to="/salas">Salas</RouterLink>
+            <RouterLink :class="linkClass('/salas')" to="/salas">Salas</RouterLink>
           </li>
           <li>
-            <RouterLink to="/recursos">Recursos</RouterLink>
+            <RouterLink :class="linkClass('/recursos')" to="/recursos">Recursos</RouterLink>
           </li>
         </ul>
       </div>
+      <label class="swap swap-rotate px-5">
+        <input type="checkbox" value="dark" class="theme-controller" />
+        <span class="swap-off icon-[tabler--sun] size-7"></span>
+        <span class="swap-on icon-[tabler--moon] size-7"></span>
+      </label>
       <button @click="handleLogout" class="btn max-md:btn-square btn-primary" href="#">
         <span class="max-md:hidden">Sair</span>
       </button>
