1fa1aeede852c7c11f4aff2e15b5e8e4393a99dd
Lucas Gabriel <lucasg.psilva03@gmail.com>
2025-12-12 10:38:05 -0300

    cria: cria middleware de tratamento de erros

---
+++ agendamento-back/middlewares/errorMiddleware.js
@@ -0,0 +1,16 @@
+module.exports = (err, req, res, next) => {
+    console.log('chegou no middleware')
+    console.error(err);
+
+    if (err.statusCode) {
+        return res.status(err.statusCode).json({
+            status: "error",
+            message: err.message,
+        });
+    }
+
+    res.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+    });
+};
